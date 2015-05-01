a21f492937d54bb8822759c4c3d0285f35a96e24
Roland Maio <rolandmaio38@gmail.com>
2015-04-30 21:49:55 -0400

    Finished! Works for n = 2 to 6. I am creating and pushing to the branch roland-110 so that everyone else can still tackle the problem!

--- pascalSorting.cpp
+++ pascalSorting.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
@@ -14,13 +15,23 @@ int main(){
   string s = "a,b,c,d,e,f";
 
   cin >> n;
+  if(n == 1){
+
+    cerr << "Valid values of n are: 2 <= n <= 6\n";
+    exit(1);
+
+  }
+
   s = s.substr(0,2*n-1);
+
   cout << "program sort(input,output);" << endl;
   cout << "var" << endl;
   cout << s << " : integer;" << endl;
   cout << "begin" << endl;
   cout << "  readln(" << s << ");" << endl;
-  printCases("a", 1, n);
+
+  printCases("a", n);
+
   cout << "end." << endl;
 
   return 0;
@@ -31,7 +42,7 @@ void printCases(string knownOrdering, int maxDepth){
 
     int currentDepth = knownOrdering.length() + 1;
     char var = depthToVar(currentDepth);
-    string indent = string(currentDepth, "  ");
+    string indent = string(2 * currentDepth, ' ');
 
     // Check for base case.
     if(currentDepth == maxDepth){
@@ -44,19 +55,19 @@ void printCases(string knownOrdering, int maxDepth){
              << makeArguments(knownOrdering, var, 0) << ")\n";
 
         // Print the else if statements. //
-        for(int i = 2; i < maxDepth; ++i){
+        for(int i = 3; i < maxDepth + 1; ++i){
 
             cout << indent << "else if "
                  << knownOrdering.at(maxDepth - i)
                  << " < " << var << " then\n";
             cout << indent << "  " << "writeln("
-                 << makeArguments(knownOrdering, var, i - 1) << ")\n";
+                 << makeArguments(knownOrdering, var, i - 2) << ")\n";
 
         }
 
         // Print the else statement. //
         cout << indent << "else\n";
-        cout << indent << "  " << << "writeln("
+        cout << indent << "  " << "writeln("
              << makeArguments(knownOrdering, var, maxDepth - 1) << ")\n";
 
         return;
@@ -69,16 +80,16 @@ void printCases(string knownOrdering, int maxDepth){
     printCases(knownOrdering + var, maxDepth);
 
     // Print the else if comparison cases.
-    for(int i = 1; i < currentDepth - 2; ++i){
+    for(int i = 1; i <= currentDepth - 2; ++i){
 
-        cout << indent << "else if " << knownOrdering.at(currentDepth - (i + 1))
-             << " < " << var << "then\n";
+        cout << indent << "else if " << knownOrdering.at(currentDepth - (i + 2))
+             << " < " << var << " then\n";
         printCases(makeOrdering(knownOrdering, var, i), maxDepth);
 
     }
 
     // Print the else case.
-    cout << indent << "else\n"
+    cout << indent << "else\n";
     printCases(var + knownOrdering, maxDepth);
 
     return;
@@ -92,14 +103,40 @@ void printCases(string knownOrdering, int maxDepth){
  */
 string makeArguments(string knownOrdering, char var, int varRightIndex){
 
-    int varLeftIndex = 
+    int varLeftIndex = knownOrdering.length() - varRightIndex,
+        i;
+    string result = "";
+
+    for(i = 0; i < varLeftIndex; ++i){
+        result = result + knownOrdering[i] + ",";
+    }
+
+    result = result + var;
+
+    for(; i < knownOrdering.length(); ++i){
+        result = result + "," + knownOrdering[i];
+    }
+
+    return result;
 
 }
 
 char depthToVar(int currentDepth){
 
+    char vars[6] = {'x', 'b', 'c', 'd', 'e', 'f'};
+
+    return vars[currentDepth - 1];
+
 }
 
 string makeOrdering(string knownOrdering, char var, int rightIndex){
+
+    int leftIndex = knownOrdering.length() - rightIndex;
+
+    string result = knownOrdering.substr(0, leftIndex) +
+                    var +
+                    knownOrdering.substr(leftIndex);
+
+    return result;
 
 }
