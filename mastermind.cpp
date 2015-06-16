2c1d0de3c33ab0f2a916e4d80241dd4f5d197430
Allen Kim <akim003@citymail.cuny.edu>
2015-06-15 23:05:12 -0600

    Finished naive implementation of mastermind

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 
 using namespace std;
+
+bool increment(int* A, int numPin, int numColor);
 
 int main(){
   int numGames;
@@ -13,22 +16,81 @@ int main(){
     cin >> numPin;
     cin >> numColor;
     cin >> numGuess;
-    vector<int*> guesses;
-    vector<int*> guesses2;
+    vector<vector<int> > guesses;
+    vector<vector<int> > guesses2;
+    vector<int> blackColors;
+    vector<int> totalColors;
     for (int k = 0; k < numGuess; ++k){
-      int guess[numPin];
-      int guess2[numColor];
-      for (int j = 0; j < numColor; guess[j++] = 0);//Initialize guess2
+      vector<int> guess(numPin);
+      vector<int> guess2(numColor);
+      for (int j = 0; j < numColor; guess2[j++] = 0);//Initialize guess2
       for (int j = 0; j < numPin; ++j){
         int value;
         cin >> value;
-        guess[j] = value;
-        guess2[value]++;
+        guess[j] = value-1;
+        guess2[value-1]++;
       }
       guesses.push_back(guess);
       guesses2.push_back(guess2);
+      int color1, color2;
+      cin >> color1;
+      blackColors.push_back(color1);
+      cin >> color2;
+      totalColors.push_back(color1 + color2);
     }
-    int currentGuess[numPin];
+    int guessToCheck[numPin];
+    for (int j = 0; j < numPin; guessToCheck[j++] = 0);
+    bool cheating = true;
+    do{
+      int histogram[numColor];
+      for (int j = 0; j < numColor; histogram[j++] = 0);
+      for (int j = 0; j < numPin; ++j){ histogram[guessToCheck[j]]++; }
+      bool goodGuess = true;
+      for (int j = 0; j < numGuess; ++j){
+        int sumBW = 0;
+        for (int k = 0; k < numColor; k++){
+          if (histogram[k] < guesses2[j][k])
+            sumBW += histogram[k];
+          else
+            sumBW += guesses2[j][k];
+        }
+        if (sumBW != totalColors[j]){
+          goodGuess = false;
+          break;
+        }
+        int blackCount = 0;
+        for (int k = 0; k < numPin; ++k){
+          if (guessToCheck[k] == guesses[j][k])
+            blackCount++;
+        }
+        if (blackCount != blackColors[j]){
+          goodGuess = false;
+          break;
+        }
+      }
+      if (goodGuess){
+        for (int j = 0; j < numPin; ++j)
+          cout << guessToCheck[j]+1 << " ";
+        cout << endl;
+        cheating = false;
+        break;
+      }
+    }while (increment(guessToCheck, numPin, numColor));
+    if (cheating)
+      cout << "You are cheating!" << endl;
   }
   return 0;
+}
+
+bool increment(int* A, int numPin, int numColor){
+  if (numPin <= 0)
+    return false;
+  if (A[numPin-1] < numColor-1){
+    A[numPin-1]++;
+    return true;
+  }
+  else if (A[numPin-1] == numColor-1){
+    A[numPin-1] = 0;
+    return increment(A, numPin-1, numColor);
+  }
 }
