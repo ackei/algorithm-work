f5eaa773f882ca2dbe08bc518f98d1790da5e38a
Roland Maio <rolandmaio38@gmail.com>
2015-10-26 12:58:30 -0400

    preparing to change to 1-dimensional dp table.

--- 10465-HomerSimpson/homer.cpp
+++ 10465-HomerSimpson/homer.cpp
@@ -3,21 +3,23 @@
 #include<algorithm>
 #include<cstring>
 #include<map>
+#include<utility>
 using namespace std;
 
-short memo[10000][10000];
+map<pair<short, short>, short> memo;
 short m, n, t, bestIdx, bestTr;
 
 short eat(short p, short r){
     if(r < m && r < n) return r >= 0 ? r : 0;
-    if(memo[p][r] != -1) return memo[p][r];
-    short &ans = memo[p][r] = numeric_limits<short>::max();
+    if(memo.count(pair<short, short>(p, r))) return memo[pair<short, short>(p, r)];
+    short ans = numeric_limits<short>::max();
     if(r >= m) ans = min(ans, eat(p+1, r-m));
     if(r >= n) ans = min(ans, eat(p+1, r-n));
     if(ans < bestTr || (ans == bestTr && bestIdx < p)){
         bestTr = ans;
         bestIdx = p;
     }
+    memo.insert(pair<pair<short, short>, short>(pair<short,short>(p, r), ans));
     return ans;
 }
 
@@ -26,7 +28,7 @@ int main(){
     while(scanf("%hd %hd %hd", &m, &n, &t) != EOF){
         bestIdx = 0;
         bestTr = numeric_limits<short>::max();
-        fill(&memo[0][0], &memo[0][0] + 10000*10000, -1);
+        memo.clear();
         short tr = eat(1, t);
         if(tr){
             printf("%hd %hd\n", bestIdx, tr);
