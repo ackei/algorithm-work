4b27446e8bec1ba9e884772790bd48d99810df7e
Roland Maio <rolandmaio38@gmail.com>
2016-03-13 12:59:22 -0500

    Rocking Robot!

--- APAC-2016/Round-E/Problem-B-Robot-Rock-Band/rock-roland.cpp
+++ APAC-2016/Round-E/Problem-B-Robot-Rock-Band/rock-roland.cpp
@@ -3,30 +3,36 @@
 #include<map>
 using namespace std;
 
+#define F first
+#define S second
+
+typedef unsigned long long int ull;
+typedef map<int, ull>::iterator miter;
+
 int main(){
     int T, C=1, N, K, r;
     scanf("%d", &T);
     while(T--){
-        vector<int> lists[4]; int ctr = 0; map<int, int> memo;
+        //printf("Processing test case %d\n", T);
+        map<int, ull> lists[4], plists[2]; ull ctr = 0;
         scanf("%d %d", &N, &K);
-        for(int j = 0; j < N; ++j){
-            scanf("%d", &r); lists[0].push_back(r); memo[r] = 0;
+        for(int i = 0; i < 4; ++i) for(int j = 0; j < N; ++j){
+            scanf("%d", &r);
+            if(lists[i].count(r)) ++lists[i][r];
+            else lists[i][r] = 1;
         }
-        for(int i = 1; i < 4; ++i) for(int j = 0; j < N; ++j){
-            scanf("%d", &r); lists[i].push_back(r);
-        }
-        for(int i = 0; i < N; ++i){
-            if(memo[lists[0][i]]){
-                ctr += memo[lists[0][i]];
-                continue;
-            }
-            for(int j = 0; j < N; ++j) for(int k = 0; k < N; ++k) for(int m = 0; m < N; ++m)
-                if((lists[0][i]^lists[1][j]^lists[2][k]^lists[3][m]) == K){
-                    ++memo[lists[0][i]];
-                    ++ctr;
-                    //printf("%d %d %d %d\n", i, j, k, m);
-                }
-        }
-        printf("Case #%d: %d\n", C++, ctr);
+        for(miter a = lists[0].begin(); a != lists[0].end(); ++a)
+            for(miter b = lists[1].begin(); b != lists[1].end(); ++b)
+                if(plists[0].count(a->F^b->F)) plists[0][a->F^b->F] += a->S*b->S;
+                else plists[0][a->F^b->F] = a->S*b->S;
+        for(miter c = lists[2].begin(); c != lists[2].end(); ++c)
+            for(miter d = lists[3].begin(); d != lists[3].end(); ++d)
+                if(plists[1].count(c->F^d->F^K)) plists[1][c->F^d->F^K] += c->S*d->S;
+                else plists[1][c->F^d->F^K] = c->S*d->S;
+
+        for(miter it = plists[0].begin(); it != plists[0].end(); ++it)
+            if(plists[1].count(it->F)) ctr += it->S*plists[1][it->F];
+
+        printf("Case #%d: %llu\n", C++, ctr);
     }
 }
